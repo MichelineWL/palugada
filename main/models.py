bbc7fde1b0661dcc4518b54585a2932795894b64
MichelineWL <michelinewijaya@gmail.com>
2024-09-11 06:33:03 +0700

    make urls

--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/', default='default.jpg')
+
+    @property
+    def __str__(self):
+        return self.name  # makes admin and shell outputs more readable
