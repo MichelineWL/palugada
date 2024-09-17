8c04d9ca0c62fa2a3c379bc20557a22e28c3e871
MichelineWL <michelinewijaya@gmail.com>
2024-09-18 06:19:29 +0700

    feat: json and xml

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product_entry, show_xml
+from main.views import show_main, create_product_entry, show_xml, show_xml_by_id, show_json, show_json_by_id
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +9,9 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product-entry', create_product_entry, name='create_product_entry'),
     path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
 
 if settings.DEBUG:
