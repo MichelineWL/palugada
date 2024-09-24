fa61f56ff553e328d4d13726221754d1749fd7ba
MichelineWL <michelinewijaya@gmail.com>
2024-09-24 13:13:08 +0700

    feat: restrick access to main page, collect data from cookies

--- main/views.py
+++ main/views.py
@@ -1,13 +1,17 @@
+import datetime
 from django.shortcuts import render, redirect
 from main.forms import ProductEntryForm
 from main.models import Product
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     product = Product.objects.all()
 
@@ -20,6 +24,7 @@ def show_main(request):
         'nama_pemilik' : 'Micheline Wijaya Limbergh',
         'kelas_pemilik' : 'PBP D',
         'product_entries' : product,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -76,8 +81,9 @@ def login_user(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('main:show_main')
-        
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
     else:
         form = AuthenticationForm(request)
     context = {'form': form}
@@ -85,4 +91,6 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return redirect('main:login')
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
