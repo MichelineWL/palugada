c0bd37573bc94bc74bd5c6647245c78c7bd55e6c
MichelineWL <michelinewijaya@gmail.com>
2024-10-02 07:27:32 +0700

    feat: edit and delete functionality, add whitenoise, and responsive navbar

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product_entry, show_xml, show_xml_by_id, show_json, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, create_product_entry, show_xml, show_xml_by_id, show_json, show_json_by_id, register, login_user, logout_user, edit_product, delete_product
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -15,6 +15,8 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<uuid:id>', edit_product, name='edit_product'), 
+    path('delete-product/<uuid:id>', delete_product, name='delete_product'), 
 
 ]
 
